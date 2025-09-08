720efe4f17e56147cc798069fc6de9a78176fe46
TerrellAW <terraw1999@proton.me>
2025-09-08 14:31:01 -0600

    Add: Basic documentation

--- src/modernappliances/application/Appliance.java
+++ src/modernappliances/application/Appliance.java
@@ -1,9 +1,10 @@
 package modernappliances.application;
 
 /**
- * 
+ * Abstract Appliance super-class that is extended by the specific appliance types of Modern Appliances.
+ * Implements the toString() method so it can be overridden by sub-classes.
  * @author mr-bones
- * @version 2025-09-05
+ * @version 08-09-2025
  */
 public abstract class Appliance {
 
