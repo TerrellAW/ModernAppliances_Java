e00874ba433979132774893e60e89742fd4f1544
TerrellAW <terraw1999@proton.me>
2025-09-08 16:07:08 -0600

    Fix: Properly override toString() and expanded javadoc, attributes

--- src/modernappliances/application/Appliance.java
+++ src/modernappliances/application/Appliance.java
@@ -7,19 +7,79 @@ package modernappliances.application;
  */
 public abstract class Appliance {
 	
+	// Attributes
+	private int itemNumber;
+	private String brand;
+	private int quantity;
+	private int wattage;
+	private String color;
+	private double price;
+	
+	// Getters and Setters
+	public int getItemNumber() {
+		return itemNumber;
+	}
+
+	public void setItemNumber(int itemNumber) {
+		this.itemNumber = itemNumber;
+	}
+	
+	public String getBrand() {
+		return brand;
+	}
+
+	public void setBrand(String brand) {
+		this.brand = brand;
+	}
+
+	public int getQuantity() {
+		return quantity;
+	}
+
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+
+	public int getWattage() {
+		return wattage;
+	}
+
+	public void setWattage(int wattage) {
+		this.wattage = wattage;
+	}
+
+	public String getColor() {
+		return color;
+	}
+
+	public void setColor(String color) {
+		this.color = color;
+	}
+
+	public double getPrice() {
+		return price;
+	}
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
+
 	// Constructor
 	public Appliance() {
 		
 	}
 
+	// Methods
 	/**
 	 * Implemented so it can be overridden by sub-classes.
 	 * Sub-classes will concatenate their specific toString outputs to this super-class one.
 	 * @author mr-bones
 	 * @version 08-09-2025
 	 */
+	@Override
 	public String toString() {
-		return "";
+		return "Item Number: " + getItemNumber() + "Brand: " + getBrand() + "Quantity: " + getQuantity() 
+				+ "Wattage: " + getWattage() + "Colour: " + getColor() + "Price: " + getPrice();
 	}
 	
 }
