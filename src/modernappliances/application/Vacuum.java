69dfa620cac84368cd374cd5bd48ce7b510867cc
TerrellAW <terraw1999@proton.me>
2025-09-09 16:39:43 -0600

    Add: Implemented Vacuum

--- src/modernappliances/application/Vacuum.java
+++ src/modernappliances/application/Vacuum.java
@@ -1,18 +1,50 @@
 package modernappliances.application;
 
 /**
- * 
+ * Vacuum class extends Appliance and implements its own unique attributes and methods.
+ * @author mr-bones
+ * @version 09-09-2025
  */
 public class Vacuum extends Appliance {
 	
-	// Constructor
-	public Vacuum() {
-		super(); // Call from super-class
+	// Attributes
+	private String grade;
+	private Voltage voltage;
+	
+	// Getters and Setters
+	public String getGrade() {
+		return grade;
 	}
 
+	public void setGrade(String grade) {
+		this.grade = grade;
+	}
+
+	public Voltage getVoltage() {
+		return voltage;
+	}
+
+	public void setVoltage(Voltage voltage) {
+		this.voltage = voltage;
+	}
+
+	// Constructor
+	public Vacuum(int itemNumber, String brand, int quantity, int wattage, String color, double price, String grade, Voltage voltage) {
+		super(itemNumber, brand, quantity, wattage, color, price); // Call from super-class
+		this.setGrade(grade);
+		this.setVoltage(voltage);
+	}
+
+	/**
+	 * Returns a string with all the appliance's attributes.
+	 * Concatenates to the Appliance super-class' implementation, which it overrides.
+	 * @author mr-bones
+	 * @version 09-09-2025
+	 * @return formatted string with appliance attributes
+	 */
 	@Override
 	public String toString() {
-		return "";
+		return super.toString() + "Grade: " + getGrade() + "Voltage: " + getVoltage();
 	}
 	
 }
