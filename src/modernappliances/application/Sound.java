f42628980cbc533a244d7d9950a7bf9e5fc66c8e
TerrellAW <terraw1999@proton.me>
2025-09-09 15:56:01 -0600

    Add: Sound enum

---
+++ src/modernappliances/application/Sound.java
@@ -0,0 +1,13 @@
+package modernappliances.application;
+
+/**
+ * Enumerates the valid sound ratings for the Dishwasher class
+ * @author mr-bones
+ * @version 09-09-2025
+ */
+public enum Sound {
+	Qt,
+	Qr,
+	Qu,
+	M
+}
