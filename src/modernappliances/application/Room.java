876d13e094e7ac394a2e23245ad9a0f5236d121d
TerrellAW <terraw1999@proton.me>
2025-09-09 16:11:21 -0600

    Add: Room enum

---
+++ src/modernappliances/application/Room.java
@@ -0,0 +1,11 @@
+package modernappliances.application;
+
+/**
+ * Enumerates the valid room types for the Microwave class
+ * @author mr-bones
+ * @version 09-09-2025
+ */
+public enum Room {
+	K,
+	W
+}
