b1040b688d0919911bbb171df4714a8b7f8158bf
TerrellAW <terraw1999@proton.me>
2025-09-13 17:08:07 -0600

    Add: Implemented enums

--- src/modernappliances/application/enumerator/Room.java
+++ src/modernappliances/application/enumerator/Room.java
@@ -3,9 +3,41 @@ package modernappliances.application.enumerator;
 /**
  * Enumerates the valid room types for the Microwave class
  * @author mr-bones
- * @version 09-09-2025
+ * @version 13-09-2025
  */
 public enum Room {
-	K,
-	W
+	// Values
+	K('K'),
+	W('W');
+	
+	// Attributes
+	private final char roomRating;
+	
+	// Getter
+	public char getRoomRating() {
+		return roomRating;
+	}
+	
+	// Constructor
+	private Room(char roomRating) {
+		this.roomRating = roomRating;
+	}
+	
+	// Methods
+	/**
+	 * Takes a char and matches it to an enum value. If it matches one of the values it is converted to an enum.
+	 * Otherwise it returns an error.
+	 * @author mr-bones
+	 * @version 13-09-2025
+	 * @param roomChar
+	 * @return valid room enum
+	 */
+	public static Room setRoomRating(char roomChar) {
+		for (Room room : values()) {
+			if (room.roomRating == roomChar) {
+				return room;
+			}
+		}
+		throw new IllegalArgumentException("No room type with " + roomChar + " rating");
+	}
 }
--- src/modernappliances/application/enumerator/Sound.java
+++ src/modernappliances/application/enumerator/Sound.java
@@ -3,11 +3,43 @@ package modernappliances.application.enumerator;
 /**
  * Enumerates the valid sound ratings for the Dishwasher class
  * @author mr-bones
- * @version 09-09-2025
+ * @version 13-09-2025
  */
 public enum Sound {
-	Qt,
-	Qr,
-	Qu,
-	M
+	// Values
+	Qt("Qt"),
+	Qr("Qr"),
+	Qu("Qu"),
+	M("M");
+	
+	// Attributes
+	private final String soundRating;
+	
+	// Getter
+	public String getSoundRating() {
+		return soundRating;
+	}
+	
+	// Constructor
+	private Sound(String soundRating) {
+		this.soundRating = soundRating;
+	}
+	
+	// Methods
+	/**
+	 * Takes a string and matches it to an enum value. If it matches one of the values it is converted to an enum.
+	 * Otherwise it returns an error.
+	 * @author mr-bones
+	 * @version 13-09-2025
+	 * @param soundStr
+	 * @return valid sound enum
+	 */
+	public static Sound setSoundRating(String soundStr) {
+		for (Sound sound : values()) {
+			if (sound.soundRating == soundStr) {
+				return sound;
+			}
+		}
+		throw new IllegalArgumentException("No sound type with " + soundStr + " rating");
+	}
 }
